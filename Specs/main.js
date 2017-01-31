36cbfb18411954adff815f0abd7b77a1f708c355
Helen Yau <helenyau13@gmail.com>
2017-01-31 15:54:23 -0800

    finish code for stringIndices and disemvoweling

--- Specs/main.js
+++ Specs/main.js
@@ -1,8 +1,9 @@
 function random() {
-  var ran = (Math.floor(Math.random() * 100) +1)
+  const ran = (Math.floor(Math.random() * 100) +1)
   console.log(ran)
-  var userInput = prompt("What's your guess of the number?", "")
-  var guessNum = parseInt(userInput)
+  let userInput = prompt("What's your guess of the number?", "")
+  let guessNum = parseInt(userInput)
+
   if(guessNum === ran) {
     alert("you won!")
   } else if(guessNum < ran) {
@@ -17,12 +18,42 @@ function random() {
 }
 
 
+var splitWords;
+var checkIndex;
+var num;
+
 function stringIndices() {
-  var wordsToParse = prompt("Enter words to parse", "")
-  var change = document.getElementById("change").innerHTML = wordsToParse
-  var regEx = /[\W_]/g
+  let wordsToParse = prompt("Enter words to parse", "")
+  let regEx = wordsToParse.replace(/[W_]/g, " ")
+  let change = document.getElementById("change").innerHTML = wordsToParse
+  splitWords = regEx.split(" ")
 }
 
+
 function checkTheIndices() {
+  checkIndex = prompt("give a number to check its index", "")
+  num = parseInt(checkIndex)
+
+  var textNode = document.createTextNode(splitWords[num - 1])
+  let li = document.createElement('li')
+  li.appendChild(textNode)
+  document.getElementById("change").appendChild(li)
+}
+
+
+function disemvowel() {
+  let wordsToDisemvowel = document.getElementById("words")
+  let lowerWords = (wordsToDisemvowel.value).toLowerCase()
+  let regEx = lowerWords.replace(/[0-9\W_*$]/g, "")
+  let emptyArray = []
+
+  for(let i = 0; i < regEx.length; i++) {
+    if(regEx[i] !== "a" && regEx[i] !== "e" && regEx[i] !== "i" && regEx[i] !== "u" && regEx[i] !== "o") {
+      emptyArray.push(regEx[i])
+    }
+  }
+
+  let disemvowel = emptyArray.join("")
+  return alert(disemvowel)
 
 }
